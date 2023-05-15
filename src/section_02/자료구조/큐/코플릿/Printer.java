51a7ac3bc3e30961e2246bf2f5417e87d436acfb
Jeonni <jeonniu@naver.com>
2023-05-15 10:28:58 +0900

    Feat: 리팩토링

--- src/section_02/자료구조/큐/코플릿/Printer.java
+++ src/section_02/자료구조/큐/코플릿/Printer.java
@@ -31,20 +31,20 @@ public class Printer {
         queue.poll();
         queue.add(documents[0]);
         // document 배열의 0번째 인덱스 삭제
-        documents = Arrays.copyOfRange(documents, 1, documents.length);
+        documents = getDocuments(documents);
         count++;
 
         // 반복 시작하기 (더 이상 문서가 없을 경우 && 대기열이 존재하지 않을 경우) => 멈추기
         while (documents.length != 0 || queue.stream().reduce(0, Integer::sum) != 0) {
             if (documents.length != 0) { // 대기 중 문서가 남은 경우
-                int sum = queue.stream().reduce(0, Integer::sum) + documents[0];
+                int sum = getSum(documents, queue);
                 if (sum > capacities) { // 작업 중인 문서와, 남은 문서 중 가장 처음 문서의 크기가 capacities보다 큰 경우
                     queue.poll();
-                    sum = queue.stream().reduce(0, Integer::sum) + documents[0];
+                    sum = getSum(documents, queue);
 
                     if (sum <= capacities) {  // 문서가 제거되고, 남은 대기열의 첫 번째 문서를 포함했을 때 수용이 가능하다면,
                         queue.add(documents[0]);
-                        documents = Arrays.copyOfRange(documents, 1, documents.length); // 문서를 하나 지워줌
+                        documents = getDocuments(documents); // 문서를 하나 지워줌
                         count++;
                     } else { // 문서가 제거되고, 남은 대기열의 첫 번째 문서를 포함했을 때 수용이 불가능하다면,
                         queue.add(0);
@@ -53,7 +53,7 @@ public class Printer {
                 } else { // 작업 중인 문서와, 남은 문서 중 가장 처음 문서의 크기가 capacities보다 작은 경우
                     queue.poll();
                     queue.add(documents[0]);
-                    documents = Arrays.copyOfRange(documents, 1, documents.length); // 문서를 하나 지워줌
+                    documents = getDocuments(documents); // 문서를 하나 지워줌
                     count++;
                 }
 
@@ -64,5 +64,13 @@ public class Printer {
             }
         }
         return count;
+    }
+
+    private static int[] getDocuments(int[] documents) {
+        return Arrays.copyOfRange(documents, 1, documents.length);
+    }
+
+    private static int getSum(int[] documents, Queue<Integer> queue) {
+        return queue.stream().reduce(0, Integer::sum) + documents[0];
     }
 }
