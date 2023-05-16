2adb976c1f37c8a601862a459e5c8e2500f7da82
Jeonni <jeonniu@naver.com>
2023-05-16 09:51:01 +0900

    Feat: 큐-Printer 문제 리팩토링

--- src/section_02/자료구조/큐/코플릿/Printer.java
+++ src/section_02/자료구조/큐/코플릿/Printer.java
@@ -35,33 +35,20 @@ public class Printer {
         count++;
 
         // 반복 시작하기 (더 이상 문서가 없을 경우 && 대기열이 존재하지 않을 경우) => 멈추기
-        while (documents.length != 0 || queue.stream().reduce(0, Integer::sum) != 0) {
+        while (documents.length != 0 || getSum(documents, queue) != 0) {
+            queue.poll();
             if (documents.length != 0) { // 대기 중 문서가 남은 경우
-                int sum = getSum(documents, queue);
+                int sum = getSum(documents, queue) + documents[0];
                 if (sum > capacities) { // 작업 중인 문서와, 남은 문서 중 가장 처음 문서의 크기가 capacities보다 큰 경우
-                    queue.poll();
-                    sum = getSum(documents, queue);
-
-                    if (sum <= capacities) {  // 문서가 제거되고, 남은 대기열의 첫 번째 문서를 포함했을 때 수용이 가능하다면,
-                        queue.add(documents[0]);
-                        documents = getDocuments(documents); // 문서를 하나 지워줌
-                        count++;
-                    } else { // 문서가 제거되고, 남은 대기열의 첫 번째 문서를 포함했을 때 수용이 불가능하다면,
-                        queue.add(0);
-                        count++;
-                    }
+                    queue.add(0);
                 } else { // 작업 중인 문서와, 남은 문서 중 가장 처음 문서의 크기가 capacities보다 작은 경우
-                    queue.poll();
                     queue.add(documents[0]);
                     documents = getDocuments(documents); // 문서를 하나 지워줌
-                    count++;
                 }
-
             } else { // 대기 중인 문서가 없는 경우
-                queue.poll();
                 queue.add(0);
-                count++;
             }
+            count++;
         }
         return count;
     }
@@ -71,6 +58,6 @@ public class Printer {
     }
 
     private static int getSum(int[] documents, Queue<Integer> queue) {
-        return queue.stream().reduce(0, Integer::sum) + documents[0];
+        return queue.stream().reduce(0, Integer::sum);
     }
 }
